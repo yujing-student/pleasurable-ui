f08cd17ef5d2a254a6f08e7b1e396f610776fe8a
yujing <medischdossier03@gmail.com>
2024-05-16 12:12:49 +0200

    built: opmaak users en toevoegen data aan de users dit is niet dymimisch

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', async function (request, response) {
     lists.data["5"].description
 
     // console.log(JSON.stringify(lists.data)+' dit is de lijst')
-    console.log(JSON.stringify(lists.data["5"].description)+' dit is de description van  1 specififke lijst')
+    console.log(JSON.stringify(lists.data["5"].description) + ' dit is de description van  1 specififke lijst')
     response.render('index', {
         alleHuizen: huizenHome.data,
         alleRatings: feedback.data,
@@ -119,30 +119,15 @@ app.get('/score/:id', function (request, response) {
         fetchJson(houseUrl)
     ])
         .then(async (feedback) => {
-            const feedbackdetails = feedback[0]; // Assuming feedback is directly an array of objects
-            const house = feedback[1]; // Assuming house data is in the second response
+            const feedbackdetails = feedback[0].data; // Assuming feedback is directly an array of objects
+            const house = feedback[1].data; // Assuming house data is in the second response
 
-            //
-            // console.log(JSON.stringify(feedbackdetails))
-            // console.log(JSON.stringify(house))
-            //
-            // // console.log(feedback);
-            //
-            //
-            // console.log(feedback[0].data[0].id+'dit is het id');
-            // console.log(feedback[0].data[0].note+'dit is de notitie');
-            // console.log(feedback[0].data[0].rating.ligging+'dit is de beoordeling en house is een object met daarin weer keys en values')
-            // console.log(feedback[0].data[0].rating.algemeen+'dit is de beoordeling en house is een object met daarin weer keys en values')
-            //
-            // feedback.data["42"].rating
-            // // feedback["1"].data.id
-            // // Render the data with the arrays
-            console.log(JSON.stringify(feedback[0]))
+            console.log(JSON.stringify(feedbackdetails[2].rating))
             response.render('score', {
-                house: feedback[1].data,
-                feedback: feedback[0],
-                rating: feedback[0].data[2].rating,//de rating klopt bij het huis maar is nu handmatig gedaan
-                notities: feedback[0].data[2].note,
+                house: house,
+                feedback: feedbackdetails,
+                ratingYuJing: feedbackdetails[2].rating,//de rating klopt bij het huis maar is nu handmatig gedaan maar dit moet dynamisch
+                notities: feedbackdetails[2].note,
                 succed: gelukt,
                 users: usersUrl.data,
             });
@@ -198,30 +183,6 @@ app.post('/score/:id', async function (request, response) {
 
 
 
-// dit is voor de sterren raten naar database
-app.post('/radio/:id', function (request, response) {
-    console.log(request.body)
-
-    // posten naar directus..
-    fetch(`${apiUrl}f_feedback/`, {
-        method: 'POST',
-        body: JSON.stringify({
-            house: request.body.id,
-            list: 12,
-            user: 7,
-            rating: {
-                stars: request.body.stars,
-            },
-        }),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-        },
-    }).then((postResponse) => {
-        console.log(postResponse)
-            response.redirect(303, '/radio/' + request.params.id)
-    })
-})
-
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8001)
 
@@ -268,3 +229,133 @@ app.get('/radio/:id', function (request, response) {
 // <!--    <li> <%= list[0].rating.kitchen %></li>-->
 //
 // <!--    <% }) %>-->
+
+// dit word niet meer gebruikt
+
+app.get('/test/:id', function (request, response) {
+    const feedback = fetchJson(`https://fdnd-agency.directus.app/items/f_feedback/?fields=*.*.*.*`)
+
+    const feedbackUrl = `https://fdnd-agency.directus.app/items/f_feedback/?fields=`;
+    const houseUrl = `https://fdnd-agency.directus.app/items/f_houses/${request.params.id}/?fields=*.*`;
+
+    Promise.all([
+        fetchJson(feedbackUrl),
+        fetchJson(houseUrl)
+    ])
+        .then(async (feedback) => {
+            const feedbackdetails = feedback[0].data; // dit s de beoordeling
+            const house = feedback[1].data; // dit is het huis die de gebruiker uitgekozen heeft
+
+
+            const targetHouseId = feedback[1].data.house_nr; // Replace with the actual house ID you want to filter for
+            // todo hier moet een foreach loop komen
+            feedbackdetails.forEach(function (house, feedback) {
+                console.log("huis gegevens:", house);
+                console.log("feedback object:", feedback);
+
+                //
+                console.log("feedback user:", feedback); //
+
+            });
+
+            //
+            // const filteredFeedback = feedbackdetails.filter(feedbackdetails => {
+            //     return feedbackdetails[0].house === targetHouseId;
+            // });
+
+            // console.log(filteredFeedback);
+
+            // console.log(JSON.stringify(test) + 'dit is de filter')
+            // console.log(JSON.stringify(feedbackdetails.data[0]))
+            //
+            // console.log(JSON.stringify(feedbackdetails[0].house))
+            // console.log(JSON.stringify(feedbackdetails[0].note))
+            // console.log(JSON.stringify(feedbackdetails[0].rating.ligging))
+            // // house
+            // console.log(JSON.stringify(feedback[1].data.house_nr))
+            // console.log(JSON.stringify(feedback[1].data.id))
+            // console.log(JSON.stringify(feedback[1].data.street))
+            // console.log(JSON.stringify(feedback[1].data.city))
+            response.render('oud/test', {
+                house: house,
+                feedback: feedbackdetails,
+                rating: feedbackdetails[2].rating,//de rating klopt bij het huis maar is nu handmatig gedaan
+                notities: feedbackdetails[2].note,
+                succed: gelukt,
+                users: usersUrl.data,
+            });
+        })
+})
+
+
+app.post('/test/:id', async function (request, response) {
+//this is the empty object
+
+    const newScore = {
+        general: request.body.algemeenNumber,
+        kitchen: request.body.keukenNumber,
+        bathroom: request.body.badkamerNumber,
+        garden: request.body.tuinNumber,
+        new: request.body.new,
+    };
+    const note = {note: request.body.note}
+// make the post route
+    fetch(`https://fdnd-agency.directus.app/items/f_feedback/?fields=*.*.*.*`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json', // Set appropriate header
+        },
+        body: JSON.stringify({
+            "house": request.params.id,
+            "list": 7,
+            "user": 5,
+            rating: newScore,
+            note: note,
+        }),
+    })
+        .then(async (apiResponse) => {
+            // if the enhanced is true do this en the render is the partial
+            if (request.body.enhanced) {
+                response.render('../partials/showScore', {
+                        result: apiResponse,
+                        succed: gelukt,
+                        //     todo hier nog een repsonse.bdy met tekst 'uw huis is tegevoegd'
+                    }
+                )
+            }
+            // the else is commented because if it is not working the full page is show in the beoordeling
+
+            // else {
+            //     response.redirect(303, '/score/' + request.params.id)
+            // }
+
+        })
+        .catch(error => {
+            console.error('Error making POST request:', error);
+        });
+})
+
+
+// dit is voor de sterren raten naar database
+app.post('/radio/:id', function (request, response) {
+    console.log(request.body)
+
+    // posten naar directus..
+    fetch(`${apiUrl}f_feedback/`, {
+        method: 'POST',
+        body: JSON.stringify({
+            house: request.body.id,
+            list: 12,
+            user: 7,
+            rating: {
+                stars: request.body.stars,
+            },
+        }),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+    }).then((postResponse) => {
+        console.log(postResponse)
+        response.redirect(303, '/radio/' + request.params.id)
+    })
+})
